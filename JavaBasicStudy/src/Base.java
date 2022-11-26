640756798f6934f15f388d7f7476ecbabe291fea
erenhuseyin <37180505+erenhuseyin@users.noreply.github.com>
2022-11-26 22:14:39 +0300

    +java exercises

--- JavaBasicStudy/src/Base.java
+++ JavaBasicStudy/src/Base.java
@@ -1,8 +1,11 @@
 public class Base {
+
+
     public static void main(String[] args) {
 
         boolean flag = true;
-        char ch = 'a';//tek tırnak
+        char mychar = 'a';//tek tırnak
+        char Ch = 'b';
         byte b = 12;
         short s = 24;
         int i = 257;
@@ -15,14 +18,19 @@ public class Base {
             flag = true; -> initialize(atama işlemi)
          */
 
+        int a = 5;
+        System.out.println("a değeri: "+a);
+
         System.out.println("flag = " + flag);
-        System.out.println("ch = " + ch);
+        System.out.println("ch = " + mychar);
+        System.out.println("ch = " + Ch);
         System.out.println("b = " + b);
         System.out.println("s = " + s);
         System.out.println("i = " + i);
         System.out.println("l = " + l);
         System.out.println("f = " + f);
         System.out.println("d = " + d);
+        System.out.println("asd\tfghj");
 
     }
 }

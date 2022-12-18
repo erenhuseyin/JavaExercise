42e9f000ec73fc598b7c882a7ff833421f9d0cde
erenhuseyin <37180505+erenhuseyin@users.noreply.github.com>
2022-12-18 14:39:05 +0300

    intro oop

---
+++ JavaBasicStudy/src/lessonFour/IntroductionOop.java
@@ -0,0 +1,36 @@
+package lessonFour;
+
+public class IntroductionOop {
+    public static class RectangularPrism{
+        double width;
+        double height;
+        double depth;
+
+        double getVolume(){//hacim
+            return width*height*depth;
+        }
+        double getArea(){//alan
+            return 2*(width*height+width*depth+height*depth);
+        }
+
+        public RectangularPrism(double width, double height, double depth) {
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+        }
+    }
+
+    public static void main(String[] args) {
+        RectangularPrism rectangular = new RectangularPrism();
+        rectangular.width = 3;
+        rectangular.height = 4;
+        rectangular.depth = 5;
+
+
+
+        double volume = rectangular.getVolume();
+        double area = rectangular.getArea();
+        System.out.println(volume);
+        System.out.println(area);
+    }
+}
